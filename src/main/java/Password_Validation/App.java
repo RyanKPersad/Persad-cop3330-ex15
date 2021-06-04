8ca2981a906820a99b3c692dd8b83fa2dfae4562
ryanpersad <Rpersad0305@gmail.com>
2021-06-04 05:59:00 -0400

    Initial commit

---
+++ src/main/java/Password_Validation/App.java
@@ -0,0 +1,30 @@
+package Password_Validation;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 15 Solution
+ *  Copyright 2021 Ryan Persad
+ */
+
+public class App 
+{
+    static  Scanner myObj = new Scanner(System.in);
+
+    public static void main( String[] args )
+    {
+        String Password = "TigerKing/2950";
+        String input;
+
+        System.out.println( "What is the Password?" );
+        input = myObj.nextLine();
+
+        if (input.equals(Password)){
+            System.out.println( "Welcome!" );
+
+        }else {
+
+            System.out.println( "I don't know you." );
+        }
+
+
+    }
+}
